51a84286d3947aa03c09403fba7d15074be2a167
Michael J. Giarlo <mjgiarlo@stanford.edu>
2018-04-12 16:44:35 -0700

    Add scopes for different organization types and add instance methods for getting back parent and child orgs

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,29 @@
 class Organization < ApplicationRecord
+  scope :universities, -> do
+    where("data ->> 'type' = :type", type: 'ROOT')
+  end
+
+  scope :schools, -> do
+    where("data ->> 'type' = :type", type: 'SCHOOL')
+  end
+
+  scope :divisions, -> do
+    where("data ->> 'type' = :type", type: 'DIVISION')
+  end
+
+  scope :subdivisions, -> do
+    where("data ->> 'type' = :type", type: 'SUB_DIVISION')
+  end
+
+  scope :departments, -> do
+    where("data ->> 'type' = :type", type: 'DEPARTMENT')
+  end
+
+  def parent
+    self.class.where("data ->> 'id' = :parent_id", parent_id: data['parent']).first
+  end
+
+  def children
+    self.class.where("data ->> 'parent' = :parent_id", parent_id: data['id'])
+  end
 end
